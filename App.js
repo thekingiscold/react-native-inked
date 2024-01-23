a2d84211171853ca071a44cd52e19400d004bbde
Samrat Mukherjee <samrat.mukherjee@sap.com>
2024-01-23 22:51:20 +0530

    added welcome screen

--- App.js
+++ App.js
@@ -1,41 +1,16 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { StatusBar, View } from 'react-native';
+import WelcomeScreen from './components/WelcomeScreen';
 
-// Screen components
-function HomeScreen({ navigation }) {
+const App = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Details')}>
-        <Text>Go to Details</Text>
-      </TouchableOpacity>
-    </View>
+    <>
+      <View style={{ flex: 1 }}>
+        <StatusBar backgroundColor="white" barStyle="dark-content" />
+        <WelcomeScreen/>
+      </View>
+    </>
   );
-}
+};
 
-function DetailsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-        <Text>Go to Home</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
 
-// Create a stack navigator
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+export default App;
